1f03425d21597385999b2c29657740e13eb8a803
Charles Fei <charles.fei@vanderbilt.edu>
2015-06-25 11:16:30 -0500

    reset

---
+++ Generate_BCS.R
@@ -0,0 +1,37 @@
+generate_BCS <- function(file, colStart = 1, colEnd = max(count.fields(file, skip = colStart)))
+{
+  
+  mydata <- read.table(file = file, header = TRUE)[, colStart:colEnd]
+  bcsData <- data.frame("Strain" = character())
+  
+  for(i in 1:ncol(mydata))
+  {
+    baseCount <- new.env(hash = TRUE)
+    total <- 0
+    
+    for(j in 1:ncol(mydata))
+    {
+      
+      if(exists(as.character(mydata[ i,j ]), where = baseCount))
+      {
+        baseCount[[ as.character(mydata[ i,j ]) ]] <- 
+          baseCount[[ as.character(mydata[ i,j ]) ]] + 1
+      }
+      else
+      {
+        if(!(as.character(mydata[ i,j ]) %in% colnames(bcsData)))
+        {
+          bcsData[[ as.character(mydata) ]] <- 0
+        }
+        baseCount[[ as.character(mydata[ i,j ]) ]] <- 1
+      }
+      total <- total + 1
+    }
+    
+    for(k in ls(baseCount))
+    {
+      bcsData[ i,k ] <- baseCount[[ as.character(k) ]]
+    }
+  }
+  bcsData
+}
