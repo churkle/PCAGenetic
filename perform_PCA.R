184221ecf6d5ca70d17c6f4c3a9e8ab054d07439
Charles Fei <charles.fei@vanderbilt.edu>
2015-07-01 15:13:34 -0500

    Finished writing PCA function

--- perform_PCA.R
+++ perform_PCA.R
@@ -1,5 +1,24 @@
-perform_PCA <- function(data)
+perform_PCA <- function(file, colStart = 1, colEnd = max(count.fields(file, skip = colStart)), alleleCol = "alleles")
 {
-    output <- prcomp(data)
+    mydata <- read.table(file = file, header = TRUE, colClasses = "character")
+    bases <- mydata[ colStart:colEnd ]
+
+    for(i in 1:nrow(mydata))
+    {
+        letterSplit <- strsplit(mydata[ i,alleleCol ], split = "/")
+        letterSplit <- sort(letterSplit[[1]])
+        baseVect <- character(3)
+        baseVect[1] <- paste(letterSplit[1], letterSplit[1], sep = "")
+        baseVect[2] <- paste(letterSplit[1], letterSplit[2], sep = "")
+        baseVect[3] <- paste(letterSplit[2], letterSplit[2], sep = "")
+        
+        for(j in colStart:ncol(mydata))
+        {
+            bases[ i,j - colStart + 1 ] <- match(as.character(mydata[ i,j ]), baseVect) - 1
+        }
+    }
     
+    bases <- data.matrix(bases)
+    bases <- t(bases)
+    prcomp(bases)
 }
