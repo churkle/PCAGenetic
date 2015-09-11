4c24484909672a9fb758b9ba3fab66086781b364
Charles Fei <charles.fei@vanderbilt.edu>
2015-09-11 14:16:56 -0500

    Added plots and documentation to source files

--- Generate_BCS.R
+++ Generate_BCS.R
@@ -1,57 +1,82 @@
+#' Generate BCS Function
+#' 
+#' This function generates BCS for an hmp file and generates a histogram
+#' @param file The hmp file to generate BCS for
+#' @param colStart The column to start counting base data from
+#' @param colEnd The last column to count
+#' @examples
+#' generate_BCS("test_files/H1k_0_01.hmp", 12)
+
 generate_BCS <- function(file, colStart = 1, colEnd = max(count.fields(file, skip = colStart)))
 {
-  
+  #Read data from file and put it into a data frame
   mydata <- read.table(file = file, header = TRUE)[, colStart:colEnd]
-  bcsData <- data.frame("Strain" = character(0), stringsAsFactors = FALSE)
+  
+  #Create data frame to put results into
+  bcsData <- data.frame("Strain" = character(0), "A" = numeric(0), "T" = numeric(0), 
+                        "G" = numeric(0), "C" = numeric(0), stringsAsFactors = FALSE)
 
+  #Go through every column in the data
   for(i in 1:ncol(mydata))
   {
+    #Give the result data frame the correct column names
     bcsData[i, "Strain"] <- colnames(mydata)[i]
     bcsData[i, !names(bcsData) %in% "Strain"] <- 0
-    baseCount <- list()
-    total <- 0
     
+    #The total of each base
+    total <- 0
+    aCount <- 0
+    tCount <- 0
+    gCount <- 0
+    cCount <- 0
+    
+    #Go through each row in the data
     for(j in 1:nrow(mydata))
     {
+      #Split double-based data into separate characters
       letters <- strsplit(as.character(mydata[ j,i ]), split = "")
       
+      #Loop through each character in the double-based data
       for(g in 1:length(letters[[1]]))
       {
-          if(exists(as.character(letters[[1]][g]), where = baseCount))
+          #Increment the total for each base character if it matches
+          if(letters[[1]][g] == "A")
           {
-            if(!(as.character(letters[[1]][g])) %in% colnames(bcsData))
-            {
-              bcsData[[ as.character(letters[[1]][g]) ]] <- 0
-            }
-            
-            baseCount[[ as.character(letters[[1]][g]) ]] <- 
-              baseCount[[ as.character(letters[[1]][g]) ]] + 1
+              aCount <- aCount + 1
           }
-          else
+          else if(letters[[1]][g] == "T")
           {
-            if(!(as.character(letters[[1]][g])) %in% colnames(bcsData))
-            {
-              bcsData[[ as.character(letters[[1]][g]) ]] <- 0
-            }
-            
-            baseCount[[ as.character(letters[[1]][g]) ]] <- 1
-            
+              tCount <- tCount + 1
           }
+          else if(letters[[1]][g] == "G")
+          {
+              gCount <- gCount + 1
+          }
+          else if(letters[[1]][g] == "C")
+          {
+              cCount <- cCount + 1
+          }
+          
+          #Increment the total amount of characters
           total <- total + 1
       }
     
     }
-    
-    for(k in names(baseCount))
-    {
-      bcsData[[ i,k ]] <- baseCount[[ as.character(k)]] / total
-    }
+        
+    #Find the BCS by dividing base frequency by the total amount
+    bcsData[[ i,"A" ]] <- aCount / total
+    bcsData[[ i,"T" ]] <- tCount / total
+    bcsData[[ i,"G" ]] <- gCount / total
+    bcsData[[ i,"C" ]] <- cCount / total
     
     bcsData[ i,"Total" ] <- total
     
   }
   
+  #Write the output data to a text file
   capture.output( print(bcsData, print.gap = 1, row.names = FALSE, right = F), 
                   file = "output.txt" )
-  hist(bcsData["A"], xlab = "A")
+  
+  #Generate a histogram of the BCS data
+  hist(bcsData[ ,"A" ], xlab = "A", breaks = 200)
 }
--- perform_PCA.R
+++ perform_PCA.R
@@ -1,6 +1,19 @@
+#'Perform PCA
+#'
+#'This function performs PCA on a hmp file and 
+#'compares the PC BCS data to the PCA scores
+#'@param file The hmp file that contains genomic data
+#'@param colStart The column to start reading data from
+#'@param colEnd The last column to read data from
+#'@param alleleCol The name of the column that contains the allele for the row
+#'@param numPCs The number of PCs to generate
+#'@examples
+#'perform_PCA("test_files/H1k_0_01.hmp", 12)
+
 perform_PCA <- function(file, colStart = 1, colEnd = max(count.fields(file, skip = colStart)), 
                         alleleCol = "alleles", numPCs = 3)
 {
+    #Angle thresholds for each PC
     pc1ThetaMin <- 30
     pc1ThetaMax <- 150
     pc1PhiMin <- -30
@@ -9,37 +22,51 @@ perform_PCA <- function(file, colStart = 1, colEnd = max(count.fields(file, skip
     pc2PhiMax <- 210
     pc3Max <- 30
   
+    #Read data from file and put it into a data frame
     mydata <- read.table(file = file, header = TRUE, colClasses = "character")
     bases <- mydata[ colStart:colEnd ]
-    pcSorted <- data.frame("PC1" = character(0), "PC2" = character(0),
-                           "PC3" = character(0), stringsAsFactors = FALSE)
+    
+    #Data frame for holding results
+    bcsData <- data.frame("A" = numeric(0), "T" = numeric(0), 
+                          "G" = numeric(0), "C" = numeric(0), stringsAsFactors = FALSE)
+    
+    #Data frame for holding correlations
+    correlations <- data.frame("Rotation" = character(0), "R1" = numeric(0),
+                               "R2" = numeric(0), "R3" = numeric(0),
+                               "R Total" = numeric(0), stringsAsFactors = FALSE)
+    rotationNum <- 1
 
-
+    #Loop through every row in the data
     for(i in 1:nrow(mydata))
     {
+        #Split multiple letter data into separate characters and sort them 
+        #alphabetically
         letterSplit <- strsplit(mydata[ i,alleleCol ], split = "/")
         letterSplit <- sort(letterSplit[[1]])
+        
+        #Find the 3 variations of the possible combinations and arrange them
+        #alphabetically
         baseVect <- character(3)
         baseVect[1] <- paste(letterSplit[1], letterSplit[1], sep = "")
         baseVect[2] <- paste(letterSplit[1], letterSplit[2], sep = "")
         baseVect[3] <- paste(letterSplit[2], letterSplit[2], sep = "")
         
+        #Iterate through the data columns and convert the character combination to a number
         for(j in colStart:ncol(mydata))
         {
           bases[ i,j - colStart + 1 ] <- match(as.character(mydata[ i,j ]), baseVect) - 1
         }
     }
     
+    #Transpose data and perform PCA on it
     bases <- data.matrix(bases)
     bases <- t(bases)
     myPCA <- prcomp(bases)
-    capture.output( print(myPCA$rotation, print.gap = 1, row.names = TRUE, right = F), file = "loading.txt" )
-    capture.output( print(myPCA$x, print.gap = 1, row.names = FALSE, right = F), file = "scores.txt" )
     
     loading <- myPCA$rotation[ ,1:3]
     score <- myPCA$x
-    #pcaRegion <- matrix(nrow = nrow(loading), ncol = 3, dimnames = list(NULL, c("PC1", "PC2", "PC3")))
     
+    #Generate Euler Rotations
     for(k in seq(0, 180, 30))
     {
       bAngle <- (k / 180) * pi
@@ -50,14 +77,24 @@ perform_PCA <- function(file, colStart = 1, colEnd = max(count.fields(file, skip
         
         for(g in seq(0, 180, 30))
         {
-          numPC1 <- 1
-          numPC2 <- 1
-          numPC3 <- 1
+          #Set all BCS totals to 0
+          pc1Total <- 0
+          pc2Total <- 0
+          pc3Total <- 0
+          pc1ATotal <- 0
+          pc1TTotal <- 0
+          pc1GTotal <- 0
+          pc1CTotal <- 0
+          pc2ATotal <- 0
+          pc2TTotal <- 0
+          pc2GTotal <- 0
+          pc2CTotal <- 0
+          pc3ATotal <- 0
+          pc3TTotal <- 0
+          pc3GTotal <- 0
+          pc3CTotal <- 0
           
-          pc1Col <- character()
-          pc2Col <- character()
-          pc3Col <- character()
-          
+          #Rotate the matrix by the Euler rotations
           dAngle <- (p / 180) * pi
           bMatrix <- matrix(c(1, 0, 0, 0, cos(bAngle), -sin(bAngle), 0, sin(bAngle), cos(bAngle)), 
                             nrow = 3, ncol = 3)
@@ -68,14 +105,17 @@ perform_PCA <- function(file, colStart = 1, colEnd = max(count.fields(file, skip
           dcMatrix <- dMatrix %*% cMatrix
           dcbMatrix <- dcMatrix %*% bMatrix
 
+          #Find the new loading of the matrix and remove zero values
           newLoading <- loading %*% dcbMatrix
           row_sub <- apply(newLoading, 1, function(row) all(row !=0 ))
           newLoading <- newLoading[row_sub,]
           newMyData <- mydata[row_sub,]
+          
+          #Create matrix to hold spherical coordinates of rotated values
           spherical <- matrix(nrow = nrow(newLoading), ncol = 3, 
                               dimnames = list(NULL ,c("r", "theta", "phi")))
           
-
+          #Calculate spherical coordinates of the newly rotated values
           for(l in 1:nrow(newLoading))
           {
             x <- newLoading[ l,1 ]
@@ -91,47 +131,125 @@ perform_PCA <- function(file, colStart = 1, colEnd = max(count.fields(file, skip
             spherical[ l,3 ] <- phi
           }
           
+          #Find BCS for each individual PC
+          #REWRITE THIS PART
           for(t in 1:nrow(newLoading))
           {
+            #PC1
             if(spherical[ t,2 ] >= pc1ThetaMin & spherical[ t,2 ] <= pc1ThetaMax
                & spherical[ t,3 ] >= pc1PhiMin & spherical[ t,3 ] <= pc1PhiMax)
             {
-              curNumPC1 <- numPC1
-              for(m in 0:(colEnd - colStart))
-              {
-                pcSorted[ m + curNumPC1,"PC1" ] <- newMyData[ t,colStart + m ]
-                numPC1 <- numPC1 + 1
-#                 numPC1 <- numPC1 + 1
-#                 pc1Col[numPC1] <- mydata[ t,colStart + m - 1 ]
-              }
+                for(q in colStart:colEnd)
+                {
+                    letters <- strsplit(as.character(newMyData[ t,q ]), split = "")
+                    for(g in 1:length(letters[[1]]))
+                    {
+                      if(letters[[1]][g] == "A")
+                      {
+                        pc1ATotal <- pc1ATotal + 1
+                      }
+                      else if(letters[[1]][g] == "T")
+                      {
+                        pc1TTotal <- pc1TTotal + 1
+                      }
+                      else if(letters[[1]][g] == "G")
+                      {
+                        pc1GTotal <- pc1GTotal + 1
+                      }
+                      else if(letters[[1]][g] == "C")
+                      {
+                        pc1CTotal <- pc1CTotal + 1
+                      }
+                      pc1Total <- pc1Total + 1
+                    }
+                }
             }
+            #PC2
             else if(spherical[ t,2 ] >= pc1ThetaMin & spherical[ t,2 ] <= pc1ThetaMax
                     & spherical[ t,3 ] >= pc2PhiMin & spherical[ t,3 ] <= pc2PhiMax)
             {
-              curNumPC2 <- numPC2
-              for(n in 0:(colEnd - colStart))
+              for(q in colStart:colEnd)
               {
-                pcSorted[ n + curNumPC2,"PC2" ] <- newMyData[ t,colStart + n ]
-                numPC2 <- numPC2 + 1
-#                 numPC2 <- numPC2 + 1
-#                 pc1Col[numPC2] <- mydata[ t,colStart + m - 1 ]
+                letters <- strsplit(as.character(newMyData[ t,q ]), split = "")
+                for(g in 1:length(letters[[1]]))
+                {
+                  if(letters[[1]][g] == "A")
+                  {
+                    pc2ATotal <- pc2ATotal + 1
+                  }
+                  else if(letters[[1]][g] == "T")
+                  {
+                    pc2TTotal <- pc2TTotal + 1
+                  }
+                  else if(letters[[1]][g] == "G")
+                  {
+                    pc2GTotal <- pc2GTotal + 1
+                  }
+                  else if(letters[[1]][g] == "C")
+                  {
+                    pc2CTotal <- pc2CTotal + 1
+                  }
+                  pc2Total <- pc2Total + 1
+                }
               }
             }
+            #PC3
             else if(spherical[ t,2 ] <= pc3Max)
             {
-              curNumPC3 <- numPC3
-              for(o in 0:(colEnd - colStart))
+              for(q in colStart:colEnd)
               {
-                pcSorted[ o + curNumPC3,"PC3" ] <- newMyData[ t,colStart + o ]
-                numPC3 <- numPC3 + 1
-#                 numPC3 <- numPC3 + 1
-#                 pc3Col[numPC3] <- mydata[ t,colStart + m - 1 ]
+                letters <- strsplit(as.character(newMyData[ t,q ]), split = "")
+                for(g in 1:length(letters[[1]]))
+                {
+                  if(letters[[1]][g] == "A")
+                  {
+                    pc3ATotal <- pc3ATotal + 1
+                  }
+                  else if(letters[[1]][g] == "T")
+                  {
+                    pc3TTotal <- pc3TTotal + 1
+                  }
+                  else if(letters[[1]][g] == "G")
+                  {
+                    pc3GTotal <- pc3GTotal + 1
+                  }
+                  else if(letters[[1]][g] == "C")
+                  {
+                    pc3CTotal <- pc3CTotal + 1
+                  }
+                  pc3Total <- pc3Total + 1
+                }
               }
             }
           }
+          
+          #Fill data frame with BCS results from individual PCs
+          bcsData[[ "PC1","A" ]] <- pc1ATotal / pc1Total
+          bcsData[[ "PC1","T" ]] <- pc1TTotal / pc1Total
+          bcsData[[ "PC1","G" ]] <- pc1GTotal / pc1Total
+          bcsData[[ "PC1","C" ]] <- pc1CTotal / pc1Total
+          bcsData[ "PC1","Total" ] <- pc1Total
+          bcsData[[ "PC2","A" ]] <- pc2ATotal / pc2Total
+          bcsData[[ "PC2","T" ]] <- pc2TTotal / pc2Total
+          bcsData[[ "PC2","G" ]] <- pc2GTotal / pc2Total
+          bcsData[[ "PC2","C" ]] <- pc2CTotal / pc2Total
+          bcsData[ "PC2","Total" ] <- pc2Total
+          bcsData[[ "PC3","A" ]] <- pc3ATotal / pc3Total
+          bcsData[[ "PC3","T" ]] <- pc3TTotal / pc3Total
+          bcsData[[ "PC3","G" ]] <- pc3GTotal / pc3Total
+          bcsData[[ "PC3","C" ]] <- pc3CTotal / pc3Total
+          bcsData[ "PC3","Total" ] <- pc3Total
+          
+          #This needs to be fixed
+          r1 <- cor(x = bcsData[["PC1", "A"]], y = myPCA$x[["PC1"]])
+          r2 <- cor(x = bcsData[["PC2", "A"]], y = myPCA$x[["PC2"]])
+          r3 <- cor(x = bcsData[["PC3", "A"]], y = myPCA$x[["PC3"]])
+          correlations[[""]]
+          rotationNum <- rotationNum + 1
         }
       }
     }
-    generate_BCS_data(pcSorted)
+    write(bcsData, file = "PCBCData.txt")
+    bcsData
 }
 
